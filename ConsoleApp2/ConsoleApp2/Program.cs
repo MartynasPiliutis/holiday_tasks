c39a2b6efff00c9b9c4998667f36fa046c8ba77d
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-22 23:41:15 +0200

    HT savarankiskas darbas 2 Task 2 EGLUTE complete

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        protected void Display (int x, int y, string s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(s);
+        }
+        static void Main(string[] args)
+        {
+            int userNumber = 0;
+            Console.Write("Iveskite eiluciu skaiciu: ");
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            int cursorPositionX = userNumber;
+            int cursorPositionY = userNumber;
+            Console.Clear();
+            for (int i = 0; i < userNumber; i++)
+            {
+                for (int a = 0; a < i+1; a = a+1)
+                {
+                    Program p = new Program();
+                    p.Display(cursorPositionY, i, "*");
+                    cursorPositionY = cursorPositionY + 2;
+                }
+                cursorPositionY = cursorPositionX - 1;
+                cursorPositionX = cursorPositionY;
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
