0620b4cba8eeb38fec63bcb2ce63308dff11faa2
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-22 20:25:54 +0200

    ht savarankisko darbo (2) task11 complete

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        protected void display(int x, int y, string s)
+        protected void Display(int x, int y, string s)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(s);
@@ -17,14 +17,26 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+            int userNumber;
+            int suma = 0;
+            Console.Write("Iveskite bet koki skaiciu: ");
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            for (int x = 0; x <= userNumber; x++)
+            {
+                suma = suma + x;
+            }
+            Console.Clear();
+
             for (int i = 0; i <= 100; i++)
             {
-                Thread.Sleep(100);
+                Thread.Sleep(50);
                 Program p = new Program();
-                p.display(0, 0, $"{i} %");
-                p.display(i, 1, "#");
+                p.Display(0, 0, $"Skaiciuojama... {i} %");
+                p.Display(i, 1, "#");
                 
             }
+            Console.WriteLine();
+            Console.WriteLine("Suma nuo 1 iki {0} yra {1}", userNumber, suma);
             Console.ReadLine();
         }
     }
