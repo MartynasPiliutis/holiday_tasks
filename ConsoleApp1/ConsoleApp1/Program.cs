d11d7b1ef712aa142d22eb1398367d0a00fb7ed7
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-22 20:07:59 +0200

    ht savarankiskas darbas 2 task1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,8 +9,23 @@ namespace ConsoleApp1
 {
     class Program
     {
+        protected void display(int x, int y, string s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(s);
+        }
+
         static void Main(string[] args)
         {
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(100);
+                Program p = new Program();
+                p.display(0, 0, $"{i} %");
+                p.display(i, 1, "#");
+                
+            }
+            Console.ReadLine();
         }
     }
 }
